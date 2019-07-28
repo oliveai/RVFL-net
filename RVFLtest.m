a7954741e24f4df9218871f8c4e1a3c5740adcca
Apdullah <apdullahyayik@gmail.com>
2019-07-28 22:24:42 +0300

    Update RVFLtest.m

--- RVFLtest.m
+++ RVFLtest.m
@@ -1,10 +1,10 @@
-function y=RVFLtest(input, net)
+function out=RVFLtest(input, net)
 % RVFLtest: Random Vector Functional Link
 % (Test)
 %
 %
 %Output Parameters
-%         y: actul output
+%        out: actul output
 %
 %Input Parameters
 %         input: input data (each row represent different observations)
@@ -17,9 +17,8 @@ function y=RVFLtest(input, net)
 %         input=rand(3,5);
 %         target=rand(3,1);
 %         enhancementnodesneuronnumber=5
-%         [outputlayerweights, hiddenlayerweights]=...
-%             rvflmBPTrain(input, target, enhancementnodesneuronnumber)
-%         y=rvflmBPTest(input, outputlayerweights, hiddenlayerweights)
+%         net=RVFLtrain(input, target, enhancementnodesneuronnumber)
+%         out=RVFLtest(input, net)
 %        % check target and y values
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %                           TEST                               %
@@ -30,7 +29,40 @@ function y=RVFLtest(input, net)
 % %                                                              %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+input=normDadapt(input, net.normparameters.minn, net.normparameters.maxx);
 enhancementnodesoutput=logsig(input*net.hiddenlayerweights);
 hiddenlayerout=[input, enhancementnodesoutput]; % concatanation
 y=hiddenlayerout*net.outputlayerweights;
+out=outCreate(y);
 end
+
+
+
+function out=outCreate(y)
+% create output
+
+outtemp=[];
+for p=1:size(y,1)
+    outtemp=[outtemp; y(p,:)==max(y(p,:))];
+end
+clear y
+
+out=zeros(size(outtemp,1), 1);
+for pp=1:size(outtemp,2)
+    out=out+outtemp(:,pp)*pp;
+end
+end
+
+function X=normDadapt(X, minn, maxx)
+% adapt norm
+
+sizeX=size(X);
+for ii=1:sizeX(1)
+    for j=1:sizeX(2)
+        X(ii,j)=(((X(ii,j)-minn(j))/(maxx(j)-minn(j))))*2-1;
+    end
+end
+end
+
+
+
